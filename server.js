c39030e1bf24e132402020072cefaf44e261f500
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2017-06-23 14:42:51 +0530

    Add a logger that works. We have to print params

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config() // Accesses the environment variables from .env file 
 const express = require("express"),
 	createtables = require("./config/createtables.js"),
 	vogels = require('vogels'),
+    morgan = require('morgan'),
+    logger = require('./config/logger'),
     app = express(),
     port = process.env.PORT,
     bodyParser = require("body-parser");
@@ -12,9 +14,22 @@ const express = require("express"),
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode < 400
+    }, stream: process.stderr
+}));
+
+app.use(morgan('dev', {
+    skip: function (req, res) {
+        return res.statusCode >= 400
+    }, stream: process.stdout
+}));
+
 var routes = require('./api/routes/LRRoutes');
 routes(app);
 
-app.listen(port);
+app.listen(port, function(){
+    logger.info('Example app listening on port ' + port);
+});
 
-console.log('RESTful API server started on: ' + port);
